5b69579d2a14c74f2ea7e4290e1698e3dd4cad80
Rafal Swieton <rafal@eskrzynka.pl>
2018-02-12 22:46:51 +0100

    Change js/script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 // script.js
+
+function drawTree(h) {
+	for (var i = 1 ; i <= h ; i++) {
+		var star = '';
+		for (var j = 1 ; j <= h - i ; j++) {
+			star += ' ';
+		}
+		for (var k = 1 ; k <= (2 * i - 1); k++) {
+			star += '*';
+		}
+		console.log(star);
+	}
+}
+drawTree(prompt('Podaj liczbę poziomow choinki: ')); 
+
+
